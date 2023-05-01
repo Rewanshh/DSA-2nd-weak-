0a3aa56040c4518aa5cf4e7429e859360b4d4d02
Rewanshh <rewanshnagpure@gmail.com>
2023-05-01 21:27:54 +0530

    first commit

---
+++ ASSIGNMENT.java
@@ -0,0 +1,16 @@
+public class ASSIGNMENT {
+    public static void main(String args[])
+    {
+//The assigment operators are mainly used to assign the value in varieable 
+// It aaign the value  of right side to left side;
+/* int m1;
+ int m2;
+ int m3;
+ int m4;*/
+ int m1,m2,m3,m4;
+ m1=m2=m3=m4=10;
+ System.out.println(m1+" "+m2+" "+ m3 +" "+m4);
+
+    }
+    
+}
